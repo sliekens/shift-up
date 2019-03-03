78b61299668201a2188e8d8b59191042ddfe5f7c
ryparker <ryparker33@gmail.com>
2019-03-03 11:09:20 -0600

    fixxer upper
    Fixed bugs
     -note: the tsconfig was required to get the correct ECMAScript version "es2015"
    Added ability to read codes from file
    Added files that have shiftcodes

--- app.ts
+++ app.ts
@@ -1,59 +1,88 @@
-import commander, { Command } from 'commander';
-import { Response } from 'request';
-import rp from 'request-promise-native';
-import { MemoryCookieStore } from 'tough-cookie';
-import { CookieMonster } from './cookie-monster';
-import { WebClient } from './web-client';
+import commander, { Command } from "commander"
+import { Response } from "request"
+import rp from "request-promise-native"
+import { MemoryCookieStore } from "tough-cookie"
+import { CookieMonster } from "./cookie-monster"
+import { WebClient } from "./web-client"
+import fs from "fs"
 
-let cookieStore = new MemoryCookieStore();
-let jar = rp.jar(cookieStore);
+let cookieStore = new MemoryCookieStore()
+let jar = rp.jar(cookieStore)
 let http = rp.defaults({
     jar,
     headers: {
-        'dnt': '1'
+        dnt: "1"
     },
     followRedirect: resp => {
         if (resp.headers.location != null) {
-            console.log('GET', resp.headers.location);
+            console.log("GET", resp.headers.location)
         }
-        return true;
+        return true
     },
     followAllRedirects: true
-});
+})
 
-const cookieMonster = new CookieMonster(cookieStore);
-const webClient = new WebClient(http, 'https://shift.gearboxsoftware.com/');
+const cookieMonster = new CookieMonster(cookieStore)
+const webClient = new WebClient(http, "https://shift.gearboxsoftware.com/")
+const secretsDir = "./secrets"
+const codesDir = "./shiftcodes"
 
-commander.version('1.0.0');
+!fs.existsSync(secretsDir) && fs.mkdirSync(secretsDir)
 
-commander.command('login <user> <password>')
-    .action((u, p) => {
-        webClient.login(u, p)
-            .then(() => cookieMonster.showCookies())
-            .then(cookies => cookieMonster.buryCookies(cookies, `./secrets/${u}.json`));
-    });
+commander.version("1.0.0")
 
-commander.command('redeem <user> <code')
-    .action((u, c) => {
-        cookieMonster
-            .fetchCookies(`./secrets/${u}.json`)
-            .then(() => cookieMonster.isAuthenticated())
-            .then(loggedIn => {
-                if (!loggedIn) {
-                    return Promise.reject('Unauthorized');
-                }
-                return Promise.resolve(true);
-            })
-            .then(() => webClient.getRedemptionForm(c))
-            .then(formData => webClient.redeem(formData))
-            .then(result => console.log(result))
-            .catch((err: Response) => {
-                if (err && err.statusCode === 500) {
-                    console.error(err.statusMessage || 'Internal Server Error');
-                } else {
-                    console.error(err);
-                }
-            });
-    });
+commander.command("login <user> <password>").action((u, p) => {
+    webClient
+        .login(u, p)
+        .then(() => cookieMonster.showCookies())
+        .then(cookies => cookieMonster.buryCookies(cookies, `${secretsDir}/${u}.json`))
+})
 
-commander.parse(process.argv);
+commander.command("redeem <user> <code>").action((u, c) => {
+    const readCodes = () => {
+        if (c.endsWith(".txt")) {
+            return fs
+                .readFileSync(`${codesDir}/${c}`, "utf-8")
+                .split("\n")
+                .filter(Boolean)
+        } else {
+            return c
+        }
+    }
+
+    const codes: string[] = readCodes()
+
+    const wait = (ms: number) => new Promise(res => setTimeout(res, ms))
+    const processCode = (c: string) =>
+        new Promise(res =>
+            res(
+                cookieMonster
+                    .fetchCookies(`${secretsDir}/${u}.json`)
+                    .then(() => cookieMonster.isAuthenticated())
+                    .then(loggedIn => {
+                        if (!loggedIn) {
+                            return Promise.reject("Unauthorized")
+                        }
+                        return Promise.resolve(true)
+                    })
+                    .then(() => webClient.getRedemptionForm(c))
+                    .then(formData => webClient.redeem(formData))
+                    .then(result => console.log(result))
+                    .catch((err: Response) => {
+                        if (err && err.statusCode === 500) {
+                            console.error(err.statusMessage || "Internal Server Error")
+                        } else {
+                            console.error(err)
+                        }
+                    })
+            )
+        )
+    ;(async function redeemCodes() {
+        for (const c of codes) {
+            await processCode(c)
+            await wait(10000) //attempt to avoid 429 and 504 server codes
+        }
+    })()
+})
+
+commander.parse(process.argv)
