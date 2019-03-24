94caa93529e5b3c771c19eb8eda9e6af170dbf7b
Steven Liekens <steven.liekens@gmail.com>
2019-03-24 20:07:33 +0100

    ensure secrets dir exists

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import commander, { Command } from 'commander';
+import { promises as fs } from 'fs';
 import { Response } from 'request';
 import rp from 'request-promise-native';
 import { MemoryCookieStore } from 'tough-cookie';
@@ -29,6 +30,13 @@ commander.version('1.0.0');
 commander.command('login <user> <password>')
     .action((u, p) => {
         webClient.login(u, p)
+            .then(async () => {
+                try {
+                    await fs.mkdir('secrets');
+                } catch {
+                    // ignore 'EEXIST: file already exists'
+                }
+            })
             .then(() => cookieMonster.showCookies())
             .then(cookies => cookieMonster.buryCookies(cookies, `./secrets/${u}.json`));
     });
