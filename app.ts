66f96e5c3e9aa0be52f067268a45d2ecdb7d972a
Steven Liekens <steven.liekens@gmail.com>
2019-03-30 18:49:56 +0100

    use async/await

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import commander, { Command } from 'commander';
 import { promises as fs } from 'fs';
-import { Response } from 'request';
 import rp from 'request-promise-native';
 import { MemoryCookieStore } from 'tough-cookie';
 import { CookieMonster } from './cookie-monster';
@@ -24,40 +23,38 @@ let http = rp.defaults({
 });
 
 const cookieMonster = new CookieMonster(cookieStore);
-const webClient = new WebClient(http, 'https://shift.gearboxsoftware.com/');
+const webClient = new WebClient(http, 'https://shift.gearboxsoftware.com');
 
 commander.version('1.0.0');
 
 commander.command('login <user> <password>')
-    .action((u, p) => {
-        webClient.login(u, p)
-            .then(async () => {
-                try {
-                    await fs.mkdir('secrets');
-                } catch {
-                    // ignore 'EEXIST: file already exists'
-                }
-            })
-            .then(() => cookieMonster.showCookies())
-            .then(cookies => cookies.filter(cookie => cookie.key === 'si'))
-            .then(cookies => cookieMonster.buryCookies(cookies, `./secrets/${u}.json`));
+    .action(async (user, password) => {
+        await webClient.login(user, password);
+        try {
+            await fs.mkdir('secrets');
+        } catch {
+            // ignore 'EEXIST: file already exists'
+        }
+
+        let cookies = await cookieMonster.showCookies();
+        cookies = cookies.filter(cookie => cookie.key === 'si');
+        await cookieMonster.buryCookies(cookies, `./secrets/${user}.json`);
     });
 
-commander.command('redeem <user> <code')
-    .action((u, c) => {
-        cookieMonster
-            .fetchCookies(`./secrets/${u}.json`)
-            .then(() => cookieMonster.isAuthenticated())
-            .then(loggedIn => {
-                if (!loggedIn) {
-                    return Promise.reject('Unauthorized');
-                }
-                return Promise.resolve(true);
-            })
-            .then(() => webClient.getRedemptionForm(c))
-            .then(formData => webClient.redeem(formData))
-            .then(result => console.log(result))
-            .catch((err: Response) => console.error(err.message));
+commander.command('redeem <user> <code>')
+    .action(async (user, code) => {
+        await cookieMonster.fetchCookies(`./secrets/${user}.json`);
+        let loggedIn = await cookieMonster.isAuthenticated();
+        if (!loggedIn) {
+            throw new Error('Unauthorized');
+        }
+
+        try {
+            let formData = await webClient.getRedemptionForm(code);
+            let result = await webClient.redeem(formData);
+        } catch (error) {
+            console.error(error);
+        }
     });
 
 commander.parse(process.argv);
--- web-client.ts
+++ web-client.ts
@@ -2,85 +2,104 @@ import cheerio from 'cheerio';
 import { RequestAPI, RequiredUriUrl, Response } from 'request';
 import { RequestPromise, RequestPromiseOptions } from 'request-promise-native';
 
+const resources = {
+    loginForm: '/home',
+    submitLoginForm: '/sessions',
+    codeForm: '/code_redemptions/new',
+    checkCode: '/entitlement_offer_codes', // ?code=<code>
+    submitCodeForm: '/code_redemptions'
+};
+Object.freeze(resources);
+
+/**
+ * WebClient is responsible for interacting with the SHiFT website.
+ */
 export class WebClient {
+    /**
+     * @param http an object that can GET and POST data over HTTP.
+     * @param baseUrl the SHiFT website location without any path segments, i.e. 'https://shift.gearboxsoftware.com'.
+     */
     constructor(
         private http: RequestAPI<RequestPromise, RequestPromiseOptions, RequiredUriUrl>,
         private baseUrl: string
     ) {
     }
 
-    getToken(url: string): Promise<string> {
+    /**
+     * Gets a page and returns its CSRF token (A.K.A. authenticity token).
+     * @param url the location of the page.
+     */
+    async getToken(url: string): Promise<string> {
         console.log('GET', url);
-        return this.http
+        let $: CheerioStatic = await this.http
             .get({
                 uri: url,
                 transform: body => cheerio.load(body)
-            })
-            .then(($: CheerioStatic) => $('meta[name=csrf-token]').attr('content'));
-    }
-
-    login(email: string, password: string): Promise<any> {
-        return this.getToken(this.baseUrl + 'home')
-            .then((token: string) => {
-                console.log('POST', this.baseUrl + 'sessions');
-                return this.http.post({
-                    uri: this.baseUrl + 'sessions',
-                    formData: {
-                        'authenticity_token': token,
-                        'user[email]': email,
-                        'user[password]': password
-                    }
-                });
             });
+
+        return $('meta[name=csrf-token]').attr('content');
     }
 
-    getRedemptionForm(code: string): Promise<any> {
-        return this.getToken(this.baseUrl + 'code_redemptions/new')
-            .then(token => {
-                console.log('GET', `${this.baseUrl}entitlement_offer_codes?code=${code}`);
-                return this.http.get({
-                    uri: `${this.baseUrl}entitlement_offer_codes?code=${code}`,
-                    headers: {
-                        'x-csrf-token': token,
-                        'x-requested-with': 'XMLHttpRequest'
-                    },
-                    transform: body => cheerio.load(body)
-                });
-            })
-            .then(($: CheerioStatic) => {
-                if ($('form.new_archway_code_redemption').length === 0) {
-                    return Promise.reject($.root().text().trim());
-                } else {
-                    return Promise.resolve({
-                        'authenticity_token': $('input[name=authenticity_token]').val(),
-                        'archway_code_redemption[code]': $('#archway_code_redemption_code').val(),
-                        'archway_code_redemption[check]': $('#archway_code_redemption_check').val(),
-                        'archway_code_redemption[service]': $('#archway_code_redemption_service').val()
-                    });
-                }
-            });
+    /**
+     * Submits a login form and returns the entire response.
+     * @param email a valid SHiFT account name
+     * @param password a valid password
+     */
+    async login(email: string, password: string) {
+        let token = await this.getToken(this.baseUrl + resources.loginForm);
+        console.log('POST', this.baseUrl + resources.submitLoginForm);
+        return this.http.post({
+            uri: this.baseUrl + resources.submitLoginForm,
+            formData: {
+                'authenticity_token': token,
+                'user[email]': email,
+                'user[password]': password
+            }
+        });
     }
 
-    redeem(formData: any): Promise<string | null> {
-        console.log('POST', this.baseUrl + 'code_redemptions');
-        return this.http
-            .post({
-                uri: this.baseUrl + 'code_redemptions',
-                formData,
-                resolveWithFullResponse: true,
-                followRedirect: false
-            })
-            .then((response: Response) => this.checkRedemptionStatus(response))
-            .then(redirect => {
-                console.log('GET', this.baseUrl + redirect);
-                return this.http.get({
-                    uri: this.baseUrl + redirect
-                });
-            })
-            .then(body => this.getAlert(body));
+    async getRedemptionForm(code: string): Promise<any> {
+        let token = await this.getToken(this.baseUrl + resources.codeForm);
+        console.log('GET', `${this.baseUrl}${resources.checkCode}?code=${code}`);
+        let $: CheerioStatic = await this.http.get({
+            uri: `${this.baseUrl}${resources.checkCode}?code=${code}`,
+            headers: {
+                'x-csrf-token': token,
+                'x-requested-with': 'XMLHttpRequest'
+            },
+            transform: body => cheerio.load(body)
+        });
+
+        if ($('form.new_archway_code_redemption').length === 0) {
+            return Promise.reject($.root().text().trim());
+        } else {
+            return {
+                'authenticity_token': $('input[name=authenticity_token]').val(),
+                'archway_code_redemption[code]': $('#archway_code_redemption_code').val(),
+                'archway_code_redemption[check]': $('#archway_code_redemption_check').val(),
+                'archway_code_redemption[service]': $('#archway_code_redemption_service').val()
+            };
+        }
     }
 
-    checkRedemptionStatus(response: Response): Promise<string> {
+    async redeem(formData: any): Promise<string | null> {
+        console.log('POST', this.baseUrl + resources.submitCodeForm);
+        let response = await this.http.post({
+            uri: this.baseUrl + resources.submitCodeForm,
+            formData,
+            resolveWithFullResponse: true,
+            followRedirect: false
+        });
+        let redirectLocation = await this.checkRedemptionStatus(response);
+        console.log('GET', this.baseUrl + redirectLocation);
+        let body = await this.http.get({
+            uri: this.baseUrl + redirectLocation
+        });
+
+        return this.getAlert(body);
+    }
+
+    private async checkRedemptionStatus(response: Response): Promise<string> {
         if (response.statusCode === 302) {
             return Promise.resolve(response.headers.location!);
         }
@@ -93,19 +112,21 @@ export class WebClient {
         const { status, url } = this.getStatus(response.body);
         if (status != null) {
             console.log(status);
-            return this.wait(500)
-                .then(() => this.http.get({
-                    uri: this.baseUrl + url,
-                    resolveWithFullResponse: true,
-                    followRedirect: false
-                }))
-                .then(r2 => this.checkRedemptionStatus(r2));
+            await this.wait(500);
+            let nextResponse = await this.http.get({
+                uri: this.baseUrl + url,
+                resolveWithFullResponse: true,
+                followRedirect: false
+            });
+
+            // retry recursively
+            return this.checkRedemptionStatus(nextResponse);
         }
         return Promise.reject(response.body);
     }
 
-    private wait(n: number): Promise<void> {
-        return new Promise(resolve => setTimeout(resolve, n));
+    private wait(ms: number): Promise<void> {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     private getAlert(body: any): string | null {
